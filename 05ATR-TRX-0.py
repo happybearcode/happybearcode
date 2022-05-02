e4619a398973be45128f29fa8bf03ceb6aded458
happybearcode <104537650+happybearcode@users.noreply.github.com>
2022-05-03 00:16:44 +0900

    Update 05ATR-TRX-0.py

--- 05ATR-TRX-0.py
+++ 05ATR-TRX-0.py
@@ -49,7 +49,7 @@ while True:
         start_time = get_start_time("KRW-TRX")
         end_time = start_time + datetime.timedelta(days=1)
 
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
+        if start_time < now < end_time + datetime.timedelta(seconds=10):
             target_price = get_target_price("KRW-TRX", 0)
             current_price = get_current_price("KRW-TRX")        
             if target_price < current_price:
