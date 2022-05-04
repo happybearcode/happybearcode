4e5fa235cc57b889bec65521fcb5f4e49b32a599
happybearcode <104537650+happybearcode@users.noreply.github.com>
2022-05-04 23:00:38 +0900

    Update 04ATR-TRX-MA5.py

--- 04ATR-TRX-MA5.py
+++ 04ATR-TRX-MA5.py
@@ -84,19 +84,21 @@ while True:
             if target_price < current_price and ma7 < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-TRX", krw*0.9995)
-                    time.sleep(0.5)
+                    upbit.buy_market_order("KRW-TRX", krw*0.1)
+                    time.sleep(0.3)
 # 매도명령 타겟가 보다 하락시 판매
-            if target_price > (current_price + under):
+            if target_price > (current_price + (under*0.5)):
                 btc = get_balance("TRX")
                 if btc > 0:
-                    upbit.sell_market_order("KRW-TRX", btc)
+                    upbit.sell_market_order("KRW-TRX", btc*0.1)
                     max_price = target_price
+                    
         
         else:
             btc = get_balance("TRX")
             if btc > 0:
                 upbit.sell_market_order("KRW-TRX", btc)
+                
 
 #         elif (max_price < current_price):
 #              max_price = current_price
