46e4f28f1a5ef89d98d330291b45552f1d3d2125
happybearcode <104537650+happybearcode@users.noreply.github.com>
2022-05-05 00:12:21 +0900

    Update and rename 05-STEEM.py to 04-STEEM.py

--- 04-STEEM.py
+++ 04-STEEM.py
@@ -57,7 +57,7 @@ while True:
         start_time = get_start_time("KRW-STEEM")
         end_time = start_time + datetime.timedelta(minutes=30)
 
-        if start_time < now < end_time - datetime.timedelta(seconds=1):
+        if start_time < now < end_time + datetime.timedelta(seconds=1):
             target_price = get_target_price("KRW-STEEM", 0)
             ma7 = get_ma7("KRW-STEEM")
             current_price = get_current_price("KRW-STEEM")
@@ -84,20 +84,20 @@ while True:
             if target_price < current_price and ma7 < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-STEEM", krw*0.1)
+                    upbit.buy_market_order("KRW-STEEM", krw*0.9995)
                     time.sleep(0.3)
 # 매도명령 타겟가 보다 하락시 판매
             if target_price > (current_price + (under*0.5)):
                 btc = get_balance("STEEM")
                 if btc > 0:
-                    upbit.sell_market_order("KRW-STEEM", btc*0.1)
+                    upbit.sell_market_order("KRW-STEEM", btc)
                     max_price = target_price
-                    
-        
+       
         else:
             btc = get_balance("STEEM")
             if btc > 0:
-                upbit.sell_market_order("KRW-STEEM", btc)
+                if target_price > (current_price + (under*0.5)):
+                    upbit.sell_market_order("KRW-STEEM", btc)
                 
 
 #         elif (max_price < current_price):
