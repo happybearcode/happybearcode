ad97ccefa68df2796fbf1256392e7bb689b78557
happybearcode <104537650+happybearcode@users.noreply.github.com>
2022-05-18 11:58:09 +0900

    Update 02-60m-MTL.py

--- 02-60m-MTL.py
+++ 02-60m-MTL.py
@@ -1,61 +1,60 @@
 import time
 import pyupbit
 import datetime
-import numpy as np
 
 access = "U5xb4ihuULs6I9se0g467KyNnaSwVybGlyWHTwQp"
 secret = "HVowWtMp0w2FeyxiaqQqOM4tprqyPxaBfDZ9eEMx"
 
-def get_target_price(ticker, k):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
-    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
-    return target_price
+# def get_target_price(ticker, k):
+#     """변동성 돌파 전략으로 매수 목표가 조회"""
+#     df = pyupbit.get_ohlcv(ticker, interval="minute10", count=2)
+#     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
+#     return target_price
 
 # def get_range(ticker):
-#     df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
+#     df = pyupbit.get_ohlcv(ticker, interval="minute10", count=2)
 #     range = df.iloc[0]['high'] - df.iloc[0]['low']
 #     return range
 
 def get_start_time(ticker):
     """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=1)
+    df = pyupbit.get_ohlcv(ticker, interval="minute10", count=1)
     start_time = df.index[0]
     return start_time
 
-# def get_low(ticker):
-#     df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
-#     low = df.iloc[0]['low']
-#     return low
+def get_low(ticker):
+    df = pyupbit.get_ohlcv(ticker, interval="minute10", count=2)
+    low = df.iloc[0]['low']
+    return low
 
 def get_open(ticker):
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=1)
+    df = pyupbit.get_ohlcv(ticker, interval="minute10", count=1)
     open = df.iloc[0]['open']
     return open
 
 def get_ma2(ticker):
     """2일 이동 평균선 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute10", count=2)
     ma2 = df['close'].rolling(2).mean().iloc[-1]
     return ma2
 
-# def get_ma3(ticker):
-#     """3일 이동 평균선 조회"""
-#     df = pyupbit.get_ohlcv(ticker, interval="minute60", count=3)
-#     ma3 = df['close'].rolling(3).mean().iloc[-1]
-#     return ma3
+def get_ma3(ticker):
+    """3일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="minute10", count=3)
+    ma3 = df['close'].rolling(3).mean().iloc[-1]
+    return ma3
 
 def get_ma4(ticker):
     """4일 이동 평균선 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=4)
+    df = pyupbit.get_ohlcv(ticker, interval="minute10", count=4)
     ma4 = df['close'].rolling(4).mean().iloc[-1]
     return ma4
 
-def get_ma15(ticker):
-    """15일 이동 평균선 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=15)
-    ma15 = df['close'].rolling(15).mean().iloc[-1]
-    return ma15
+# def get_ma15(ticker):
+#     """15일 이동 평균선 조회"""
+#     df = pyupbit.get_ohlcv(ticker, interval="minute10", count=15)
+#     ma15 = df['close'].rolling(15).mean().iloc[-1]
+#     return ma15
 
 def get_balance(ticker):
     """잔고 조회"""
@@ -79,7 +78,7 @@ upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 buy_sell = 0
 max_price = 0
-total_few = 0
+drop_sell = 0
 totalbuy_price = 0
 buy_price = 0
 # 자동매매 시작
@@ -87,15 +86,21 @@ while True:
     try:
         now = datetime.datetime.now()
         start_time = get_start_time("KRW-MTL")
-        end_time = start_time + datetime.timedelta(minutes=60)
+        end_time = start_time + datetime.timedelta(minutes=10)
 
-        if start_time + datetime.timedelta(seconds=5) < now < end_time:
+        if start_time + datetime.timedelta(seconds=3) < now < end_time:
             current_price = get_current_price("KRW-MTL")
-            target_price = get_target_price("KRW-MTL", 0.333334)
+            # target_price = get_target_price("KRW-MTL", 0.333334)
             open = get_open("KRW-MTL")
+            low = get_low("KRW-MTL")
             ma2 = get_ma2("KRW-MTL")
+            ma3 = get_ma3("KRW-MTL")
             ma4 = get_ma4("KRW-MTL")            
-            ma15 = get_ma15("KRW-MTL")
+            # ma15 = get_ma15("KRW-MTL")
+
+            if buy_sell == 1:
+                 if max_price < current_price:
+                    max_price = current_price
 
             if (0 < current_price < 0.1):
                 under = 0.0001
@@ -119,29 +124,53 @@ while True:
                 under = 500
             elif (2000000 <= current_price):
                 under = 1000
-            
+
+# 매수 조건            
             if buy_sell == 0:
-# 매수 조건
-                if current_price >= target_price and ma4 < ma2 and ma15 < current_price and open < current_price:
+
+                if ma4 < ma3 < ma2 and current_price > open:
+                    krw = get_balance("KRW")
+                    if (krw*0.35) > 5000:
+                        upbit.buy_market_order("KRW-MTL", (krw*0.35) * 0.9995)
+                        buy_price = current_price
+                        buy_sell = 1
+                
+            elif buy_sell == 2:
+                if current_price > drop_sell and ma4 < ma2:
                     krw = get_balance("KRW")
                     if (krw*0.35) > 5000:
                         upbit.buy_market_order("KRW-MTL", (krw*0.35) * 0.9995)
                         buy_price = current_price
                         buy_sell = 1
 # 매도 조건
-            else:
-                 if open-under > current_price:
+            elif buy_sell == 1:
+                if current_price <= low:
                     btc = get_balance("MTL")
                     if btc > 0:
                         upbit.sell_market_order("KRW-MTL", btc)
                         buy_price = 0
                         max_price = 0
                         buy_sell = 0
+                        drop_sell = 0
+
+                if max_price * 0.996 < current_price and buy_price + (under*2) < current_price:
+                    btc = get_balance("MTL")
+                    if btc > 0:
+                        upbit.sell_market_order("KRW-MTL", btc)
+                        drop_sell = current_price
+                        buy_price = 0
+                        buy_sell = 2
+
+        else:
+            if buy_sell == 2:
+                drop_sell = 0
+                max_price = 0
+                buy_sell = 0
 
 
         time.sleep(1)
-        print(now,"    CP: %.1f    target_price: %.1f    Ma:  %s    under: %.1f    buy_price: %.1f    open: %.1f" %
-             (current_price, target_price, (ma2>ma4), under, buy_price, open))
+        # print(now,"    CP: %.2f    Ma:  %s    under: %.2f    buy_price: %.2f    open: %.2f    drop_sell: %.2f    sell_price: %.2f    low: %.2f" %
+        #      (current_price, (ma2>ma3>ma4), under, buy_price, open, drop_sell, max_price * 0.996, low))
   
     except Exception as e:
         print(e)
